f279c7ce8cc0f968180b5cbc0482094d694e120e
JeongYuJin <wjddbwls1230@naver.com>
2024-10-03 21:42:00 +0900

    조명추가

--- ImGround/Assets/Scripts/DayAndNight.cs
+++ ImGround/Assets/Scripts/DayAndNight.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Rendering;
 using UnityEngine;
@@ -24,7 +24,9 @@ public class DayAndNight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         transform.Rotate(Vector3.right, 0.1f * secondPerRealTime * Time.deltaTime);
+       
 
         if (transform.eulerAngles.x >= 170)
         {
@@ -49,6 +51,77 @@ public class DayAndNight : MonoBehaviour
             {
                 currentFogDensity -= 0.1f * FogDensityCalc * Time.deltaTime;
                 RenderSettings.fogDensity = currentFogDensity;
+            }
+        }
+    }
+}
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class DayAndNight : MonoBehaviour
+{
+    [SerializeField] private float secondPerRealTime;
+
+    private bool isNight = false;
+
+    [SerializeField] private float NightFogDensityCalc; // 안개 증감량 비율
+    [SerializeField] private float DayFogDensityCalc;
+    [SerializeField] private float NightFogDensity; // 밤일 때 안개 밀도
+    private float DayFogDensity; // 낮일 때 안개 밀도
+    private float currentFogDensity; // 현재 안개 밀도
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DayFogDensity = RenderSettings.fogDensity;
+        currentFogDensity = DayFogDensity; // 시작 시 현재 안개 밀도를 낮 시간으로 설정
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // x축 회전 각도를 360도 내로 유지
+        transform.Rotate(Vector3.right, 0.1f * secondPerRealTime * Time.deltaTime);
+
+        // 회전 각도를 제한 (360도를 넘어가면 다시 0도로 설정)
+        Vector3 currentRotation = transform.eulerAngles;
+        if (currentRotation.x >= 360f)
+        {
+            currentRotation.x = 0f;
+        }
+        transform.eulerAngles = currentRotation;
+
+        // 밤과 낮을 구분 (170도 이상이면 밤, 그 이외는 낮)
+        if (currentRotation.x >= 170 && currentRotation.x <= 360)
+        {
+            isNight = true;
+        }
+        else
+        {
+            isNight = false;
+        }
+
+        // 안개 밀도 조정
+        if (isNight)
+        {
+            // 현재 안개 밀도가 밤 안개 밀도보다 작으면 증가
+            if (currentFogDensity < NightFogDensity)
+            {
+                currentFogDensity += NightFogDensityCalc * Time.deltaTime;
+                RenderSettings.fogDensity = Mathf.Clamp(currentFogDensity, DayFogDensity, NightFogDensity);
+            }
+        }
+        else
+        {
+            // 현재 안개 밀도가 낮 안개 밀도보다 크면 감소
+            if (currentFogDensity > DayFogDensity)
+            {
+                currentFogDensity -= DayFogDensityCalc * Time.deltaTime;
+                RenderSettings.fogDensity = Mathf.Clamp(currentFogDensity, DayFogDensity, NightFogDensity);
             }
         }
     }
