551de48621ad357bc5818d8a6732bb4a5e1c45ac
JeongYuJin <wjddbwls1230@naver.com>
2024-10-04 02:40:17 +0900

    조명 추가

--- ImGround/Assets/Scripts/DayAndNight.cs
+++ ImGround/Assets/Scripts/DayAndNight.cs
@@ -1,62 +1,3 @@
-/*using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Rendering;
-using UnityEngine;
-
-public class DayAndNight : MonoBehaviour
-{
-    [SerializeField] private float secondPerRealTime;
-
-    private bool isNight = false;
-
-    [SerializeField] private float FogDensityCalc; //안개 증감량 비율
-
-    [SerializeField] private float NightFogDensity; //밤일 때 안개
-    private float DayFogDensity; //낮일 때 안개
-    private float currentFogDensity; //안개 계산
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        DayFogDensity = RenderSettings.fogDensity;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-        transform.Rotate(Vector3.right, 0.1f * secondPerRealTime * Time.deltaTime);
-       
-
-        if (transform.eulerAngles.x >= 170)
-        {
-            isNight = true;
-        }
-        else //if (transform.eulerAngles.x >= 340)
-        {
-            isNight = false;
-        }
-
-        if (isNight)
-        {
-            if (currentFogDensity <= NightFogDensity)
-            {
-                currentFogDensity += 0.1f * FogDensityCalc * Time.deltaTime;
-                RenderSettings.fogDensity = currentFogDensity;
-            }
-        }
-        else
-        {
-            if (currentFogDensity >= DayFogDensity)
-            {
-                currentFogDensity -= 0.1f * FogDensityCalc * Time.deltaTime;
-                RenderSettings.fogDensity = currentFogDensity;
-            }
-        }
-    }
-}
-*/
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Rendering;
@@ -66,11 +7,13 @@ public class DayAndNight : MonoBehaviour
 {
     [SerializeField] private float secondPerRealTime;
 
-    private bool isNight = false;
+    public bool isNight = false;
 
     [SerializeField] private float NightFogDensityCalc; // 안개 증감량 비율
     [SerializeField] private float DayFogDensityCalc;
+
     [SerializeField] private float NightFogDensity; // 밤일 때 안개 밀도
+
     private float DayFogDensity; // 낮일 때 안개 밀도
     private float currentFogDensity; // 현재 안개 밀도
 
@@ -78,25 +21,16 @@ public class DayAndNight : MonoBehaviour
     void Start()
     {
         DayFogDensity = RenderSettings.fogDensity;
-        currentFogDensity = DayFogDensity; // 시작 시 현재 안개 밀도를 낮 시간으로 설정
     }
 
     // Update is called once per frame
     void Update()
     {
-        // x축 회전 각도를 360도 내로 유지
-        transform.Rotate(Vector3.right, 0.1f * secondPerRealTime * Time.deltaTime);
 
-        // 회전 각도를 제한 (360도를 넘어가면 다시 0도로 설정)
-        Vector3 currentRotation = transform.eulerAngles;
-        if (currentRotation.x >= 360f)
-        {
-            currentRotation.x = 0f;
-        }
-        transform.eulerAngles = currentRotation;
-
+        transform.Rotate(0.1f * secondPerRealTime * Time.deltaTime, 0f, 0f);
+        Debug.Log(transform.rotation.eulerAngles.x);
         // 밤과 낮을 구분 (170도 이상이면 밤, 그 이외는 낮)
-        if (currentRotation.x >= 170 && currentRotation.x <= 360)
+        if (transform.eulerAngles.x >= 170 && transform.eulerAngles.x <= 360)
         {
             isNight = true;
         }
