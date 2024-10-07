90ccf560424256529f88ff6c5c95e1fb679896da
patternplane <bss011202@naver.com>
2024-10-07 17:30:18 +0900

    [FEAT] 인벤토리 및 아이템 구조 클래스 설계

---
+++ ImGround/Assets/soungsoo/Item.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Item
+{
+    public int count;
+    public int maxCount = int.MaxValue;
+    public int remainCapacity { get => (count - maxCount < 0) ? 0 : count - maxCount; }
+
+    public bool isSame(Item item)
+    {
+        throw new NotImplementedException();
+    }
+
+    private Item getCopy()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Item getDividedItems(int count)
+    {
+        if (this.count > count
+            || count < 0)
+            count = this.count;
+
+        Item divided = getCopy();
+        this.count -= count;
+        divided.count = count;
+        return divided;
+    }
+}
