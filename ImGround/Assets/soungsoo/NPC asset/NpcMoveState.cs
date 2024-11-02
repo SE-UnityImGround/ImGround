a2c8ac081780a9272d41bb6ef64e072ae896fff1
patternplane <bss011202@naver.com>
2024-11-02 17:10:49 +0900

    [FIX|REFACTOR] NPC가 한번 이동한 후 계속 멈춰있는 문제 수정 및 NPC 시선/이동 코드 분리
    FIX :
    - 게임 실행 후 NPC가 한번 이동한 이후 더 이상 이동하지 않는 문제 수정
    
    REFACTOR :
    - NpcGazer
      NPC시선처리 기능을 담당합니다.
    - NpcMover
      NPC 이동 기능을 담당합니다.
    - NpcMoveState
      NPC의 이동 상태를 표시하기 위한 Enum입니다.
    - NpcAnimationParameter
      NPC 애니메이션 관련 파라미터 이름을 관리하기 위한 클래스입니다.

---
+++ ImGround/Assets/soungsoo/NPC asset/NpcMoveState.cs
@@ -0,0 +1,15 @@
+﻿
+/// <summary>
+/// NPC의 이동 상태를 표시합니다.
+/// </summary>
+public enum NpcMoveState
+{
+    /// <summary>
+    /// 이동중인 상태입니다.
+    /// </summary>
+    MOVE,
+    /// <summary>
+    /// 이동하지 않는 상태입니다.
+    /// </summary>
+    IDLE
+}
