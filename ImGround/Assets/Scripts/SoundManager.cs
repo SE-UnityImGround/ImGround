e5911cfde1bff570f8f8b15bf809a4c23e64bedb
JeongYuJin <wjddbwls1230@naver.com>
2024-11-22 16:56:22 +0900

    시장소리 공격소리 추가

--- ImGround/Assets/Scripts/SoundManager.cs
+++ ImGround/Assets/Scripts/SoundManager.cs
@@ -4,16 +4,30 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    public AudioSource bgm;
+    public AudioSource []bgm;
+    protected bool isNight = false;
+    private DayAndNight dayAndNightScript;
+
     // Start is called before the first frame update
     void Start()
     {
-        bgm.Play();
+        bgm[0].Play();
+
+        if (!isNight)
+        {
+            bgm[1].Play();
+        }
+        else if (isNight){
+            bgm[1].Stop();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (dayAndNightScript != null)
+        {
+            isNight = dayAndNightScript.isNight; // isNight 상태 동기화
+        }
     }
 }
