0f579bc22aa1dc033ce02981cbc13703274c706b
JeongYuJin <wjddbwls1230@naver.com>
2024-11-28 05:37:23 +0900

    효과음 및 배경음 수정
    시장, 보스
    광석 프리펩 추가

---
+++ ImGround/Assets/Scripts/BossSounds.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSounds : MonoBehaviour
+{
+    public AudioSource[] effectSound;
+
+    public GameObject player; // 플레이어 오브젝트
+    public static Vector3 minBounds = new Vector3(-78, -10, -120); // x, y, z 최소값
+    public static Vector3 maxBounds = new Vector3(183, 20, 148);
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isWithinBounds = IsPlayerWithinBounds();
+
+        if (!isWithinBounds)
+        {
+            effectSound[0].Play();
+        }
+
+    }
+    private bool IsPlayerWithinBounds()
+    {
+        if (player == null) return false;
+
+        Vector3 playerPosition = player.transform.position;
+        return playerPosition.x >= minBounds.x && playerPosition.x <= maxBounds.x &&
+               playerPosition.y >= minBounds.y && playerPosition.y <= maxBounds.y &&
+               playerPosition.z >= minBounds.z && playerPosition.z <= maxBounds.z;
+    }
+}
